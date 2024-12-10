d39c581551426202f2714456f7915e0b4aa1fcdb
Tokar1evV <st8462747@stud.duikt.edu.ua>
2024-12-10 17:45:56 +0200

    Tokariev

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -16,6 +16,7 @@ public class Player extends Entity{
 
     public final int screenX;
     public final int screenY;
+    int hasKey = 0;
 
     public Player(GamePanel gp, KeyHandler keyH){
 
@@ -28,6 +29,8 @@ public class Player extends Entity{
         solidArea = new Rectangle();
         solidArea.x = 8;
         solidArea.y = 16;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
         solidArea.width = 32;
         solidArea.height = 32;
 
@@ -80,6 +83,10 @@ public class Player extends Entity{
             collisionOn = false;
             gp.cChecker.chectTile(this);
 
+            //check object collision
+             int objIndex = gp.cChecker.checkObject(this, true);
+            pickUpObject(objIndex);
+
             //if collision is false, player can move
             if(collisionOn == false){
 
@@ -103,6 +110,30 @@ public class Player extends Entity{
             }
         }
 
+    }
+
+    public void pickUpObject(int i){
+
+        if(i != 999){
+
+            String objectName = gp.obj[i].name;
+
+            switch (objectName){
+                case "Key":
+                    hasKey++;
+                    gp.obj[i] = null;
+                    System.out.println("Key: " + hasKey);
+                    break;
+                case "Door":
+                    if(hasKey > 0){
+                        gp.obj[i] = null;
+                        hasKey--;
+                    }
+                    System.out.println("Key: " + hasKey);
+                    break;
+
+            }
+        }
     }
     public void draw(Graphics2D g2){
 
